bde179415abad961ff181e8a907e4d1ec50de9db
Simbamon <sbhan9511@gmail.com>
2021-04-03 00:24:23 +0900

    Added i18next

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+  
+  function handleClick(lang){
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <div className="App">
       <p>Test</p>
+      <p onClick={()=>handleClick('en')}>English</p>
+      <p onClick={()=>handleClick('ko')}>Korean</p>      
+      <p>{t('Language.1')}</p>
+      <p>{t('Summary.1')}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import './i18next';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback = {(<div>Loading</div>)}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
