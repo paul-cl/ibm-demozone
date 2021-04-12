3a7a44f162832a00051152bec7b84517c5ff9d16
Simbamon <sbhan9511@gmail.com>
2021-04-12 19:40:03 +0900

    Updated gh-pages

--- src/i18next.js
+++ src/i18next.js
@@ -1,19 +1,34 @@
+import translationEN from "./locales/en/translation.json";
+import translationKO from "./locales/ko/translation.json";
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
+// import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
+import Backend from 'i18next-xhr-backend';
 
 const Languages = ['en', 'ko']
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  ko: {
+    translation: translationKO,
+  },
+};
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
     fallbackLng: 'en',
     debug: true,
+    resources,
     whitelist: Languages,
     
     interpolation: {
