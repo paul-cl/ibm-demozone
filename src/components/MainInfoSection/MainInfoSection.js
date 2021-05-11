bc0171bec1f34ecc790f564f6e863118267237da
c-mac <c-mac@mac.com>
2021-05-11 15:58:01 +0900

    modify main

--- src/components/MainInfoSection/MainInfoSection.js
+++ src/components/MainInfoSection/MainInfoSection.js
@@ -4,25 +4,25 @@ import { useTranslation } from 'react-i18next';
 import { Wrapper, BackgroundVid, Videosrc, TitleTextWrapper, MainTitle, MainParagraph, MainParagraph2 } from './MainInfoSection.element'
 
 const MainInfoSection = () => {
-    const { t } = useTranslation(); 
+    const { t } = useTranslation();
 
     return (
-        <>  
+        <>
             <BackgroundVid>
-                <Videosrc autoPlay loop muted src={ Video } type = 'video/mp4' />
+                <Videosrc autoPlay loop muted src={Video} type='video/mp4' />
             </BackgroundVid>
             <Wrapper>
-                
-                
+
+
                 <TitleTextWrapper>
                     {/* <MainTitle>{t('MainHome.Title1')}</MainTitle>
                     <MainTitle>{t('MainHome.Title2')}</MainTitle>
                     <TitleLine></TitleLine>
                     <MainParagraph>{t('MainHome.Description')}</MainParagraph> */}
-                    <MainTitle>{t('MainHome.Title1')}</MainTitle>
+                    <MainTitle>Satellite deploy - {t('MainHome.Title1')}</MainTitle>
                     <MainParagraph>{t('MainHome.Description1')}</MainParagraph>
                     <MainParagraph2>{t('MainHome.Description2')}</MainParagraph2>
-                </TitleTextWrapper> 
+                </TitleTextWrapper>
 
             </Wrapper>
         </>
